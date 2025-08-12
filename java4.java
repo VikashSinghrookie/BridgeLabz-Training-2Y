961a6a316046e92f4c93a6f78e9aed803430bbd0
VikashSinghrookie <vikash.singh_cs24@gla.ac.in>
2025-08-12 14:54:23 +0530

    kilometers to miles

---
+++ java4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class java4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        System.out.printf("%.2f\n",n*n);
+
+        System.out.println("Miles = "+n * 0.621371);
+    }
+}
