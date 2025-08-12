47f881bcaa5b9c5a6eae56e37cde96a2214b18c9
VikashSinghrookie <vikash.singh_cs24@gla.ac.in>
2025-08-12 14:16:30 +0530

    just changes

---
+++ java2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class java2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n1 = sc.nextDouble();
+        double n2 = sc.nextDouble();
+        double n3 = sc.nextDouble();
+        System.out.println((n1+n2+n3)/3);
+
+
+    }
+
+}
