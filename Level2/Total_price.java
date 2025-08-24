797d3cf68c029acac470eebc282fb5e659b0dce0
VikashSinghrookie <vikash.singh_cs24@gla.ac.in>
2025-08-24 17:49:28 +0530

    Create Total_price

---
+++ Level2/Total_price.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Total_price {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double up;
+        int qty;
+        System.out.print("unit price: ");
+        unitPrice = input.nextDouble();
+        System.out.print("quantity: ");
+        quantity = input.nextInt();
+        double total = up*qty;
+        System.out.println("The total purchase price is INR " + total + " if the quantity " + qty + " and unit price is INR " + up);
+    }
+}
