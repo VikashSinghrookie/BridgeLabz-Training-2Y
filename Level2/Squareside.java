6c44a29c26b27e7fbbf0a8653600c86a2f5059f8
VikashSinghrookie <vikash.singh_cs24@gla.ac.in>
2025-08-24 17:47:04 +0530

    Create Squareside

---
+++ Level2/Squareside.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Squareside {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int p;
+        System.out.print("perimeter of square: ");
+        perimeter = input.nextInt();
+        int side = p/4;
+        System.out.println("The length of the side is " + side + " whose perimeter is " + p);
+    }
+}
