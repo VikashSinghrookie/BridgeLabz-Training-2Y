066f6f2913d4ce182d421dabf23b66ecd63b3ebe
VikashSinghrookie <vikash.singh_cs24@gla.ac.in>
2025-08-24 17:16:25 +0530

    .

---
+++ Level1/pen distribution.java
@@ -0,0 +1,8 @@
+public class Pens {
+    public static void main(String[] args) {
+        int pens = 14, students = 3;
+        int ps = pens/students;
+        int rm = pens%students;
+        System.out.println("The Pen Per Student is "+ ps + " and the remaining pen not distributed is " + rm);
+    }
+}
