0b3e755ced31a520c5285da28f14c7ba12b6f24c
VikashSinghrookie <vikash.singh_cs24@gla.ac.in>
2025-08-24 17:31:54 +0530

    ''

---
+++ Level1/Feediscount.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Feediscount {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double fee, dp;
+        System.out.print("Enter fee: ");
+        fee = input.nextDouble();
+        System.out.print("Enter discount percent: ");
+        discountPercent = input.nextDouble();
+        double d = (fee * dp) / 100;
+        double finalFee = fee - d;
+        System.out.println("The discount amount is INR " + d + " and final discounted fee is INR " + finalFee);
+    }
+}
