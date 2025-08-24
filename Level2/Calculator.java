9c6aa1c3aba293a5e285db7b3e4bf6d369488310
VikashSinghrookie <vikash.singh_cs24@gla.ac.in>
2025-08-24 17:44:42 +0530

    Create Calculator

---
+++ Level2/Calculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double n1, n2;
+        number1 = input.nextDouble();
+        number2 = input.nextDouble();
+        double add= n1+n2;
+        double sub= n1-n2;
+        double mul= n1*n2;
+        double div= n1/n2;
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " + n1 + " and " + n2 + " is " + add + ", " + sub + ", " + mul + ", and " + div);
+    }
+}
